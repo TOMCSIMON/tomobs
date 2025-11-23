cf6bbceb72b477f453426d11aecda3c28ff7a82a
Tom C Simon <tomcsimon01@gmail.com>
2025-11-23 15:54:27 +0530

    Initial commit

---
+++ src/main/java/com/tomobs/tomobs/TomobsApplication.java
@@ -0,0 +1,13 @@
+package com.tomobs.tomobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomobs/tomobs/TomobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomobs.tomobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
